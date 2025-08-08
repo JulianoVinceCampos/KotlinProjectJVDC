98dd14075b15919021901a719d157c88aa4c9ce7
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2025-08-08 14:27:08 -0300

    Criação do AppRouters.kt

---
+++ composeApp/src/commonMain/kotlin/br/com/jvdc/navigation/AppRoutes.kt
@@ -0,0 +1,10 @@
+package br.com.jvdc.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed interface AppRoutes {
+    @Serializable
+    data object MoviesList: AppRoutes
+    @Serializable
+    data class MovieDetails(val id: Int): AppRoutes
+}
