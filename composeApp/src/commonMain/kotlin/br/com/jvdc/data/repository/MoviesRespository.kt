de2fe5f070a290b05bf3f259bd3b64c8afeda110
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2025-08-08 14:25:15 -0300

    Criação MoviesRepository

---
+++ composeApp/src/commonMain/kotlin/br/com/jvdc/data/repository/MoviesRespository.kt
@@ -0,0 +1,51 @@
+package br.com.jvdc.data.repository
+
+import br.com.jvdc.data.network.KtorClient
+import br.com.jvdc.domain.model.Movie
+import br.com.jvdc.domain.model.MovieSection
+import br.com.jvdc.domain.model.toModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import kotlin.math.asin
+
+class MoviesRepository(
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
+
+
+    suspend fun getMovieSections(): List<MovieSection> {
+        return withContext(ioDispatcher) {
+            val popularMoviesDeferred = async { KtorClient.getMovies("popular") }
+            val topRatedMoviesDeferred = async { KtorClient.getMovies("top_rated") }
+            val upcomingMoviesDeferred = async { KtorClient.getMovies("upcoming") }
+
+            val popularMovies = popularMoviesDeferred.await()
+            val topRatedMovies = topRatedMoviesDeferred.await()
+            val upcomingMovies = upcomingMoviesDeferred.await()
+
+            listOf(
+                MovieSection(
+                    sectionType = MovieSection.SectionType.POPULAR,
+                    movies = popularMovies.results.map {
+                        it.toModel()
+                    }
+                ),
+                MovieSection(
+                    sectionType = MovieSection.SectionType.TOP_RATED,
+                    movies = topRatedMovies.results.map {
+                        it.toModel()
+                    }
+                ),
+                MovieSection(
+                    sectionType = MovieSection.SectionType.UPCOMING,
+                    movies = upcomingMovies.results.map {
+                        it.toModel()
+                    }
+                )
+            )
+        }
+    }
+}
