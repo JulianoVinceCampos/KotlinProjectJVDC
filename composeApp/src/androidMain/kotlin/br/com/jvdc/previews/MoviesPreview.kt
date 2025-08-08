bac3682e6f7556cee3d48e52c9411267106aaab2
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2025-08-08 20:24:00 -0300

    Ajustando MoviesPreview.kt

--- composeApp/src/androidMain/kotlin/br/com/jvdc/previews/MoviesPreview.kt
+++ composeApp/src/androidMain/kotlin/br/com/jvdc/previews/MoviesPreview.kt
@@ -2,9 +2,11 @@ package br.com.jvdc.previews
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import br.com.jvdc.domain.model.MovieSection
 import br.com.jvdc.domain.model.movie1
 import br.com.jvdc.ui.components.MoviePoster
 import br.com.jvdc.ui.movies.MoviesListScreen
+import br.com.jvdc.ui.movies.MoviesListViewModel
 
 @Preview(showBackground = true)
 @Composable
@@ -18,6 +20,13 @@ private fun MoviePosterPreview() {
 @Composable
 private fun MovieListScreenPreview() {
     MoviesListScreen(
-        popularMovies = listOf(movie1)
+        moviesListState = MoviesListViewModel.MoviesListState.Success(
+            movieSections = listOf(
+                MovieSection(
+                    sectionType = MovieSection.SectionType.POPULAR,
+                    movies = listOf(movie1)
+                )
+            )
+        )
     )
 }
