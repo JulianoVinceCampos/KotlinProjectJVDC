9900d9931713ad615c125e8bf050a583b8a58fca
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2025-08-19 20:44:03 -0300

    feat: implementa navegação principal e DI no App.kt
    - Adiciona Koin para injeção de dependências dos módulos network, data e viewModel
    - Configura tema global com MoviesAppTheme
    - Implementa NavHost com rotas para lista de filmes e detalhes do filme
    - Define navegação entre telas usando compose navigation

--- composeApp/src/commonMain/kotlin/br/com/jvdc/App.kt
+++ composeApp/src/commonMain/kotlin/br/com/jvdc/App.kt
@@ -1,27 +1,48 @@
 package br.com.jvdc
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import br.com.jvdc.di.dataModule
+import br.com.jvdc.di.networkModule
+import br.com.jvdc.di.viewModelModule
 import br.com.jvdc.navigation.AppRoutes
+import br.com.jvdc.ui.moviedetail.MovieDetailRoute
 import br.com.jvdc.ui.movies.MoviesListRoute
+import br.com.jvdc.ui.theme.MoviesAppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinApplication
+
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        val navController = rememberNavController()
-        NavHost(navController, startDestination = AppRoutes.MoviesList){
-            composable<AppRoutes.MoviesList>{
-                MoviesListRoute()
+    KoinApplication(
+            application = {
+                modules(networkModule, dataModule, viewModelModule)
             }
-            composable<AppRoutes.MovieDetails>{
+    ){
+        MoviesAppTheme {
+          val navController = rememberNavController()
+          NavHost(navController, startDestination = AppRoutes.MoviesList){
+              composable<AppRoutes.MoviesList>{
+                  MoviesListRoute(
+                      navigateToMovieDetail =  { movieId ->
+                          navController.navigate(AppRoutes.MovieDetail(movieId))
+                      }
+                  )
+              }
+              composable<AppRoutes.MovieDetail>{
+                  MovieDetailRoute(
+                      navigationBack = {
+                          navController.popBackStack()
+                      }
+                  )
+              }
 
-            }
-
-        }
+          }
+      }
     }
 }
