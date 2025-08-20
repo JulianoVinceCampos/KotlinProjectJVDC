cd97664d35a312c54a7fb99f7e3df05150d65460
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2025-08-19 21:01:25 -0300

    Adiciona modelo Movie com propriedades e objetos fake
    - Cria data class Movie para representar filmes com id, título, sinopse, poster, gêneros, ano, duração, nota e elenco
    - Permite uso de listas opcionais para gêneros e membros do elenco
    - Adiciona objetos fake para facilitar testes e desenvolvimento

--- composeApp/src/commonMain/kotlin/br/com/jvdc/domain/model/Movie.kt
+++ composeApp/src/commonMain/kotlin/br/com/jvdc/domain/model/Movie.kt
@@ -1,25 +1,29 @@
 package br.com.jvdc.domain.model
 
-import br.com.jvdc.data.network.IMAGE_SMALL_BASE_URL
-import br.com.jvdc.data.network.model.MovieResponse
-
 data class Movie(
     val id: Int,
     val title: String,
     val overview: String,
     val posterUrl: String,
+    val genres: List<Genre>?,
+    val year: Int,
+    val duration: String?,
+    val rating: String,
+    val castMembers: List<CastMember>?
 )
 
-fun MovieResponse.toModel() = Movie(
-    id = this.id,
-    title = this.title,
-    overview = this.overview,
-    posterUrl = "$IMAGE_SMALL_BASE_URL${this.posterPath}"
-)
 ///fake objects
 val movie1 = Movie(
     id = 1,
     title = "A Minecraft Movie",
-    overview = "Movie Overview",
-    posterUrl = "https://example.com/poster1.jpg",
+    overview = "Movie overview",
+    posterUrl = "url",
+    genres = listOf(genre1, genre2),
+    year = 2022,
+    duration = "2h 36 min",
+    rating = "8.5",
+    castMembers = listOf(
+        castMember1,
+        castMember2,
+    ),
 )
